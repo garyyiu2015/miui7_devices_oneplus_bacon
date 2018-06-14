9290a8eadc0750f94da378edf908ff7c800f541f
garyyiu2015 <garyyiu2015@gmail.com>
2018-06-14 15:18:37 +0800

    firmware update

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -10,3 +10,13 @@ find $TARGET_FILES_DIR -name "*.odex" |xargs rm -rf
 
 # copy files
 cp -f $MIUI_DIR/file_contexts $TARGET_FILES_DIR/META/
+
+# firmware-update
+cp -r $MIUI_DIR/emmc_appsboot.mbn $TARGET_FILES_DIR/BOOTABLE_IMAGES/
+cp -r $MIUI_DIR/logo.bin $TARGET_FILES_DIR/BOOTABLE_IMAGES/
+cp -r $MIUI_DIR/NON-HLOS.bin $TARGET_FILES_DIR/BOOTABLE_IMAGES/
+cp -r $MIUI_DIR/reserve4.img $TARGET_FILES_DIR/BOOTABLE_IMAGES/
+cp -r $MIUI_DIR/rpm.mbn $TARGET_FILES_DIR/BOOTABLE_IMAGES/
+cp -r $MIUI_DIR/sbl1.mbn $TARGET_FILES_DIR/BOOTABLE_IMAGES/
+cp -r $MIUI_DIR/static_nvbk.bin $TARGET_FILES_DIR/BOOTABLE_IMAGES/
+cp -r $MIUI_DIR/tz.mbn $TARGET_FILES_DIR/BOOTABLE_IMAGES/
--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,6 @@
 import common
 import edify_generator
+import os
 
 def AddDeviceGetprop(info):
     edify = info.script
@@ -29,8 +30,32 @@ def WritePolicyConfig(info):
     except KeyError:
         print "warning: file_context missing from target;"
     
+def FirmwareUpdate(info):
+    for filename in os.listdir("miui"):
+        if not (filename.find('.mbn')==-1 and filename.find('.bin')==-1 and filename.find('reserve4')==-1):
+            data=open(os.path.join(os.getcwd(),"miui",filename)).read()
+            common.ZipWriteStr(info.output_zip, filename, data)
+    extra_img_flash = """ui_print("Writing aboot image...");
+package_extract_file("emmc_appsboot.mbn", "/dev/block/platform/msm_sdcc.1/by-name/aboot");
+ui_print("Writing modem image...");
+package_extract_file("NON-HLOS.bin", "/dev/block/platform/msm_sdcc.1/by-name/modem");
+ui_print("Writing sbl1 image...");
+package_extract_file("sbl1.mbn", "/dev/block/platform/msm_sdcc.1/by-name/sbl1");
+ui_print("Writing rpm image...");
+package_extract_file("rpm.mbn", "/dev/block/platform/msm_sdcc.1/by-name/rpm");
+ui_print("Writing tz image...");
+package_extract_file("tz.mbn", "/dev/block/platform/msm_sdcc.1/by-name/tz");
+ui_print("Writing static_nvbk image...");
+package_extract_file("static_nvbk.bin", "/dev/block/platform/msm_sdcc.1/by-name/oppostanvbk");
+ui_print("Writing logo image...");
+package_extract_file("logo.bin", "/dev/block/platform/msm_sdcc.1/by-name/LOGO");
+ui_print("Writing reserve4 image...");
+package_extract_file("reserve4.img", "/dev/block/platform/msm_sdcc.1/by-name/reserve4");"""
+    info.script.AppendExtra(extra_img_flash);
+
 def FullOTA_InstallEnd(info):
     AddDeviceGetprop(info)
     MountSystem(info)
     DeleteSystem(info)
     WritePolicyConfig(info)
+    FirmwareUpdate(info)
