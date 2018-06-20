c4eec462e40eaabda527c77dda9d3e8da6b5c0a2
garyyiu2015 <garyyiu2015@gmail.com>
2018-06-21 00:43:23 +0800

    add advanced power menu

---
+++ customize_framework.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# $1: dir for miui
+# $2: dir for original
+
+APKTOOL="$PORT_ROOT/tools/apktool --quiet"
+GIT_APPLY=$PORT_ROOT/tools/git.apply
+BUILD_OUT=out
+
+function applyPatch () {
+    for patch in `find $1 -name *.patch`
+    do
+        cp $patch out/
+        cd out
+        $GIT_APPLY `basename $patch`
+        for rej in `find $2 -name *.rej`
+        do
+          echo "Patch $patch fail"
+          exit 1
+        done
+        cd ..
+      done
+}
+
+if [ $2 = "$BUILD_OUT/android.policy" ]
+then
+	applyPatch "overlay/android.policy"
+fi
