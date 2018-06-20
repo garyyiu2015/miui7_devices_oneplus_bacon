35eb36706ad696f93c94441904f8d7d638c44e7f
garyyiu2015 <garyyiu2015@gmail.com>
2018-06-20 23:25:18 +0800

    add coloros 2.0 firmware

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -7,3 +7,7 @@ MIUI_DIR=$PWD/miui
 
 # copy files
 cp -f $MIUI_DIR/file_contexts $TARGET_FILES_DIR/META/
+# firmware-update
+cp -r $MIUI_DIR/*.mbn $TARGET_FILES_DIR/META/
+cp -r $MIUI_DIR/*.bin $TARGET_FILES_DIR/META/
+cp -r $MIUI_DIR/reserve4.img $TARGET_FILES_DIR/META/
